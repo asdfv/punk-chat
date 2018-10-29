cf6f42645a9a0e037913ab5303808ae2bd2806ad
asdfv <grodno.by@mail.ru>
2018-10-29 23:29:48 +0300

    Fix creating message

--- src/components/message/Controller.js
+++ src/components/message/Controller.js
@@ -1,25 +1,26 @@
+import mongoose from 'mongoose';
+
 import Message from './Model';
 import loggerFunc from '../../helpers/Logger';
 import GeneralChatController from '../generalChat/Controller';
 
 const log = loggerFunc(module);
+const generalChatController = new GeneralChatController();
 
 export default class MessageController {
-    constructor() {
-        this.generalChatController = new GeneralChatController();
-    }
 
     /**
      * Create message and add it to general chat
      */
     async create(ctx) {
         const message = new Message(ctx.request.body);
+        message.owner = mongoose.Types.ObjectId(message.owner);
         try {
             const savedMessage = await message.save();
-            await this.generalChatController.addMessage(savedMessage);
+            await generalChatController.addMessage(savedMessage);
             ctx.body = savedMessage;
         } catch (err) {
-            log.error(`Error saving message: ${message}: ${err}`);
+            log.error(`Error saving message: \n${message} \n${err}`);
         }
     }
 }
